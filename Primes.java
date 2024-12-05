407520bac45a5dfeddb36c68cf7a6c9fb6901a9d
ronimaimon1 <rm241298@gmail.com>
2024-12-05 18:33:19 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,23 +1,37 @@
 public class Primes {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        boolean[] primeArr = new boolean[n+1];
-        for (int i=2; i<primeArr.length; i++)
-            primeArr[i] = true;
-        for (int p=0; p<Math.sqrt(n)+1; p++) {
-            if (primeArr[p]){
-                for (int k=p+1; k<primeArr.length; k++)
-                    if (k%p==0)
-                        primeArr[k] = false;
-            }     
-        }
-        int sum = 0;
-        for (int l=0; l<primeArr.length; l++){
-            if (primeArr[l]){
-                sum++;
-                System.out.println("" + l);
-            }
-        }
-            System.out.println("There are "+sum+" primes between 2 and "+n+" ("+(sum*100)/n+"% are primes)");
-    }
+         // Parse the upper limit 'n' from the command line argument
+         int n = Integer.parseInt(args[0]);
+
+         // Create a boolean array where primeArr[i] will be true if i is prime
+         boolean[] primeArr = new boolean[n + 1];
+ 
+         // Initialize the array: assume all numbers are prime
+         for (int i = 2; i <= n; i++) {
+             primeArr[i] = true;
+         }
+ 
+         // Sieve of Eratosthenes algorithm
+         for (int p = 2; p * p <= n; p++) {
+             if (primeArr[p]) {
+                 // Mark all multiples of p as non-prime, starting from p^2
+                 for (int k = p * p; k <= n; k += p) {
+                     primeArr[k] = false;
+                 }
+             }
+         }
+ 
+         // Count and print all primes, and calculate the percentage of primes
+         int sum = 0;
+         for (int l = 2; l <= n; l++) {
+             if (primeArr[l]) {
+                 sum++;
+                 System.out.println(l); // Print the prime number
+             }
+         }
+ 
+         // Calculate and display the percentage of prime numbers as an integer
+         int percentage = (sum * 100) / n;  // Perform integer division
+         System.out.println("There are " + sum + " primes between 2 and " + n + " (" + percentage + "% are primes)");
+     }
 }
